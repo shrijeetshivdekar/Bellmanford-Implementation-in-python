0509b7d55dec2b24355c15d1a56468c3851672c6
shrijeetshivdekar <45587402+shrijeetshivdekar@users.noreply.github.com>
2019-10-23 18:49:01 +0530

    Update App.py

--- App.py
+++ App.py
@@ -18,14 +18,14 @@ node4 = Node("D")
 edge1= Edge(5,node1,node2)
 edge2= Edge(3,node2,node3)
 edge3= Edge(2,node3,node4)
-edge4= Edge(-6,node4,node2)
+edge4= Edge(-6,node2,node4)
 edge5= Edge(4,node1,node4)
 
 node1.adjacenciesList.append(edge1)
 node1.adjacenciesList.append(edge5)
+node2.adjacenciesList.append(edge4)
 node2.adjacenciesList.append(edge2)
 node3.adjacenciesList.append(edge3)
-node4.adjacenciesList.append(edge4)
 
 vertexList=[node1,node2,node3,node4]
 edgeList=[edge1,edge2,edge3,edge4,edge5]
